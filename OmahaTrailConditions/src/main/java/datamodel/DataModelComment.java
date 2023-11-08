bd9906a10712f9f151e246726042aa46255c111a
Trossman0 <trossman@unomaha.edu>
2023-11-08 17:09:08 -0600

    Finished datamodels with getters and setters

--- OmahaTrailConditions/src/main/java/datamodel/DataModelComment.java
+++ OmahaTrailConditions/src/main/java/datamodel/DataModelComment.java
@@ -9,9 +9,14 @@ import javax.persistence.Table;
 /**
  * @since J2SE-1.8
  CREATE TABLE Comment (
-  id INT NOT NULL AUTO_INCREMENT,    
-  ... ...
-  PRIMARY KEY (id));
+  comment_id INT NOT NULL AUTO_INCREMENT,
+  post_id INT NOT NULL,
+  comment_textbody VARCHAR(500),
+  comment_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  comment_report_count INT DEFAULT 0, //this should be initialized to zero when a comment is created
+  PRIMARY KEY(comment_id),
+  FOREIGN KEY(post_id) REFERENCES postTable(post_id);
+);
  */
 @Entity
 @Table(name = "Comment")
@@ -19,28 +24,81 @@ public class DataModelComment {
 
    @Id  // primary key
    @GeneratedValue(strategy = GenerationType.IDENTITY)
-   @Column(name = "id") 
-   private Integer id;
-
-   public DataModelComment() {
+   @Column(name = "comment_id") 
+   private Integer comment_id;
+   
+   @Column(name = "post_id") 
+   private Integer post_id;
+   
+   @Column(name = "comment_body") 
+   private String comment_body;
+   
+   @Column(name = "comment_timestamp") 
+   private String comment_timestamp;
+   
+   @Column(name = "comment_report_count") 
+   private Integer comment_report_count;
+   
+   
+   public DataModelComment(Integer post_id, String comment_body, String comment_timestamp, Integer comment_report_count) {
+	   this.post_id = post_id;
+	   this.comment_body = comment_body;
+	   this.comment_timestamp = comment_timestamp;
+	   this.comment_report_count = comment_report_count;
    }
 
-   public DataModelComment(Integer id) {
-      this.id = id;
-   }
-
-
-   public Integer getId() {
-      return id;
-   }
-
-   public void setId(Integer id) {
-      this.id = id;
+   public DataModelComment(Integer comment_id, Integer post_id, String comment_body, String comment_timestamp, Integer comment_report_count) {
+      this.comment_id = comment_id;
+      this.post_id = post_id;
+      this.comment_body = comment_body;
+      this.comment_timestamp = comment_timestamp;
+      this.comment_report_count = comment_report_count;
    }
 
    
-   @Override
+   
+   	public Integer getComment_id() {
+   		return comment_id;
+	}
+	
+	public void setComment_id(Integer comment_id) {
+		this.comment_id = comment_id;
+	}
+	
+	public Integer getPost_id() {
+		return post_id;
+	}
+	
+	public void setPost_id(Integer post_id) {
+		this.post_id = post_id;
+	}
+	
+	public String getComment_body() {
+		return comment_body;
+	}
+	
+	public void setComment_body(String comment_body) {
+		this.comment_body = comment_body;
+	}
+	
+	public String getComment_timestamp() {
+		return comment_timestamp;
+	}
+	
+	public void setComment_timestamp(String comment_timestamp) {
+		this.comment_timestamp = comment_timestamp;
+	}
+	
+	public Integer getComment_report_count() {
+		return comment_report_count;
+	}
+	
+	public void setComment_report_count(Integer comment_report_count) {
+		this.comment_report_count = comment_report_count;
+	}
+
+@Override
    public String toString() {
-      return "Comment: " + this.id;
+      return "Comment: " + this.comment_id + this.post_id + this.comment_body + this.comment_timestamp + this.comment_report_count;
    }
 }
--- OmahaTrailConditions/src/main/java/datamodel/DataModelPost.java
+++ OmahaTrailConditions/src/main/java/datamodel/DataModelPost.java
@@ -9,38 +9,137 @@ import javax.persistence.Table;
 /**
  * @since J2SE-1.8
  CREATE TABLE Post (
-  id INT NOT NULL AUTO_INCREMENT,    
-  ... ...
+  post_id INT NOT NULL AUTO_INCREMENT,
+  poster_alias VARCHAR(30) NOT NULL,
+  trail_name VARCHAR(30), //this is allowed to be NULL because not all posts require the trail name. We will need to check for NULL at runtime.
+  trail_score INT, //same here, allowed to be NULL.
+  post_textbody VARCHAR(500) //same here, allowed to be NULL.
+  post_report_count INT DEFAULT 0, //this should be initialized to zero when a post is created
+  post_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  event_timestamp TIMESTAMP
   PRIMARY KEY (id));
  */
+
 @Entity
 @Table(name = "Post")
 public class DataModelPost {
 
    @Id  // primary key
    @GeneratedValue(strategy = GenerationType.IDENTITY)
-   @Column(name = "id")
-   private Integer id;
+   @Column(name = "post_id")
+   private Integer post_id;
+   
+   @Column(name = "poster_alias")
+   private String poster_alias;
 
-   public DataModelPost() {
+   @Column(name = "trail_name")
+   private String trail_name;
+   
+   @Column(name = "trail_score")
+   private Integer trail_score;
+   
+   @Column(name = "post_body")
+   private String post_body;
+   
+   @Column(name = "post_report_count")
+   private Integer post_report_count;
+
+   @Column(name = "post_timestamp")
+   private String post_timestamp;
+   
+   public DataModelPost(String palias, String tname, Integer tscore, String pbody, Integer prcount, String timestamp) {
+	 this.poster_alias = palias;
+	 this.trail_name = palias;
+	 this.trail_score = tscore;
+	 this.post_body = pbody;
+     this.post_report_count = prcount;
+     this.post_timestamp = timestamp;
    }
-
-   public DataModelPost(Integer id) {
-      this.id = id;
-      
-   }
-
-   public Integer getId() {
-      return id;
-   }
-
-   public void setId(Integer id) {
-      this.id = id;
+   
+   
+   public DataModelPost(Integer id, String poster_alias, String trail_name, Integer trail_score, String post_body, Integer prcount, String timestamp) {
+      this.post_id = id;
+      this.poster_alias = poster_alias;
+      this.trail_name = trail_name;
+      this.trail_score = trail_score;
+      this.post_body = post_body;
+      this.post_report_count = prcount;
+      this.post_timestamp = timestamp;
    }
 
    
-   @Override
+   	public Integer getPost_id() {
+	   return post_id;
+	}
+	
+	
+	public void setPost_id(Integer post_id) {
+		this.post_id = post_id;
+	}
+	
+	
+	public String getPoster_alias() {
+		return poster_alias;
+	}
+	
+	
+	public void setPoster_alias(String poster_alias) {
+		this.poster_alias = poster_alias;
+	}
+	
+	
+	public String getTrail_name() {
+		return trail_name;
+	}
+	
+	
+	public void setTrail_name(String trail_name) {
+		this.trail_name = trail_name;
+	}
+	
+	
+	public Integer getTrail_score() {
+		return trail_score;
+	}
+	
+	
+	public void setTrail_score(Integer trail_score) {
+		this.trail_score = trail_score;
+	}
+	
+	
+	public String getPost_body() {
+		return post_body;
+	}
+	
+	
+	public void setPost_body(String post_body) {
+		this.post_body = post_body;
+	}
+	
+	
+	public Integer getPost_report_count() {
+		return post_report_count;
+	}
+	
+	
+	public void setPost_report_count(Integer post_report_count) {
+		this.post_report_count = post_report_count;
+	}
+	
+	
+	public String getPost_timestamp() {
+		return post_timestamp;
+	}
+	
+	
+	public void setPost_timestamp(String post_timestamp) {
+		this.post_timestamp = post_timestamp;
+	}
+
+
+@Override
    public String toString() {
-      return "Post: " + this.id;
+      return "Post: " + this.post_id + this.poster_alias + this.trail_name + this.trail_score + this.post_body + this.post_report_count + this.post_timestamp;
    }
 }
